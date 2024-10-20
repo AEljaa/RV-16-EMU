c3f1d55fddd2243a9d6a2a697e45d5789d34e220
AEljaa <adamtantalum@gmail.com>
2024-10-20 16:16:47 +0100

    Assembler needs to be tested but non psudo instrucs should work

--- as16.py
+++ as16.py
@@ -5,11 +5,33 @@ lines=[]
 lineaddr=[]
 labels={}
 
+def unsignextend(num,size,bit):
+    print(len(num)," bit number ",num)
+    if(len(num)<size):
+        num= bit * (size - len(num)) + num
+    return num
+
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    return bin(val - (1 << bits))[3:]        # compute negative value
+
 def placeReg():
     for i in range(0,len(lines)):
         for j in range(len(lines[i])-1,-1,-1):
             try:
                 lines[i][j]=regcodes[lines[i][j]] #replace the opcode with the key value if we are on it
+            except:
+                print("can't do: ",lines[i][j])
+def placeAddr():
+    for i in range(0,len(lines)):
+        for j in range(len(lines[i])-1,-1,-1):
+            try:
+                offset=(labels[lines[i][j]]-i) #replace the opcode with the key value if we are on it
+                print("Address offset" ,offset)
+                if(offset>0):
+                    lines[i][j]=unsignextend(bin(offset)[2:],7,'0')
+                else:
+                    lines[i][j]=twos_comp(int(bin(int(offset))[3:],2),7)
             except:
                 print("can't do: ",lines[i][j])
 
@@ -29,13 +51,28 @@ def preprocess(lables):
                 lines[i][j]=opcodes[lines[i][j]] #replace the opcode with the key value if we are on it
             except:
                 if(lines[i][j].find('0x') != -1): #hex values 0xaaa
-                    lines[i][j]=int(lines[i][j],16)
+                    lines[i][j]=unsignextend(bin(int(lines[i][j],16))[2:],7,'0')
                 elif(lines[i][j].find('0o') != -1): #oct values 0oaaa
-                    lines[i][j]=int(lines[i][j],8)
+                    lines[i][j]=unsignextend(bin(int(lines[i][j],8))[2:],7,'0')
                 elif(lines[i][j].find('0b') != -1): #binary values
-                    lines[i][j]=lines[i][j][2:]
-                elif(int(lines[i][j] != -1)):
-                    print("NUMBER : ",lines[i][j])
+                    lines[i][j]=unsignextend(lines[i][j][2:],7,'0')
+                elif(lines[i][j]=='lui'): #10 bit lui imm values
+                    print("LUI ",bin(int(lines[i][j+2])))
+                    lines[i][j+2]=unsignextend(bin(int(lines[i][j+2]))[2:],10,'0')
+                elif((opcodes.get(lines[i][j],0) or regcodes.get(lines[i][j],0)) == 0):
+                    if(labels.get(lines[i][j])):
+                        print("Label : ",lines[i][j])
+                        continue
+                    elif(lines[i][j]=="halt"):
+                        print("halt : ",lines[i][j])
+                    elif(lines[i][j].isdigit()):#positive numbers
+                        print("NUMBER : ",lines[i][j])
+                        lines[i][j]=unsignextend(bin(int(lines[i][j]))[2:],7,'0')
+                    elif(lines[i][j][0]=='-' and lines[i][j][1:].isdigit()):#negative numbers
+                        print("negative number",bin(int(lines[i][j]))[3:])
+                        lines[i][j]=twos_comp(int(bin(int(lines[i][j]))[3:],2),7)
+                        print("negative 7 bit binary number",lines[i][j])
+
 
 if __name__ == "__main__":
     if(len(sys.argv)!=2):
@@ -58,6 +95,8 @@ if __name__ == "__main__":
     preprocess(labels)
     # pass 2
     placeReg()
+    # PASS 3
+    placeAddr()
     print(lines)
 
 
